f31490de58f34bce87bb1e2cb6dadf4b1f7e6a06
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2022-12-27 08:03:12 +0530

    Use install-location

--- win.bat
+++ win.bat
@@ -1,12 +1,14 @@
 @echo off
 
+mkdir "C:\Software"
+
 echo Checking if VLC is already installed...
 winget show VideoLAN.VLC >nul 2>&1
 if %errorlevel% == 0 (
   echo VLC is already installed. Skipping installation.
 ) else (
   echo VLC is not installed. Installing...
-  winget install VideoLAN.VLC
+  winget install VideoLAN.VLC --install-location="C:\Software"
 )
 
 echo Checking if Chrome is already installed...
@@ -15,7 +17,7 @@ if %errorlevel% == 0 (
   echo Chrome is already installed. Skipping installation.
 ) else (
   echo Chrome is not installed. Installing...
-  winget install Google.Chrome
+  winget install Google.Chrome --install-location="C:\Software"
 )
 
 echo Checking if Firefox is already installed...
@@ -24,7 +26,7 @@ if %errorlevel% == 0 (
   echo Firefox is already installed. Skipping installation.
 ) else (
   echo Firefox is not installed. Installing...
-  winget install Mozilla.Firefox
+  winget install Mozilla.Firefox --install-location="C:\Software"
 )
 
 echo Installation complete.
