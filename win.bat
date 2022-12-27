f07822f301e85f62e0ee7f535a54be31a43973ef
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2022-12-27 07:20:10 +0530

    Condition to check if the software is already installed

--- win.bat
+++ win.bat
@@ -1,15 +1,33 @@
 @echo off
 
-echo Installing VLC...
-powershell -Command "& { (New-Object Net.WebClient).DownloadFile('https://download.videolan.org/vlc/3.0.11/win64/vlc-3.0.11-win64.exe', 'vlc-installer.exe') }"
-vlc-installer.exe /S
+echo Checking if VLC is already installed...
+reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\VLC media player" /v DisplayName >nul 2>&1
+if %errorlevel% == 1 (
+  echo VLC is not installed. Installing...
+  powershell -Command "& { (New-Object Net.WebClient).DownloadFile('https://download.videolan.org/vlc/3.0.11/win64/vlc-3.0.11-win64.exe', 'vlc-installer.exe') }"
+  vlc-installer.exe /S
+) else (
+  echo VLC is already installed. Skipping installation.
+)
 
-echo Installing Chrome...
-powershell -Command "& { (New-Object Net.WebClient).DownloadFile('https://dl.google.com/chrome/install/googlechromestandaloneenterprise64.msi', 'chrome-installer.msi') }"
-msiexec /i chrome-installer.msi /quiet
+echo Checking if Chrome is already installed...
+reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Google Chrome" /v DisplayName >nul 2>&1
+if %errorlevel% == 1 (
+  echo Chrome is not installed. Installing...
+  powershell -Command "& { (New-Object Net.WebClient).DownloadFile('https://dl.google.com/chrome/install/googlechromestandaloneenterprise64.msi', 'chrome-installer.msi') }"
+  msiexec /i chrome-installer.msi /quiet
+) else (
+  echo Chrome is already installed. Skipping installation.
+)
 
-echo Installing Firefox...
-powershell -Command "& { (New-Object Net.WebClient).DownloadFile('https://download-installer.cdn.mozilla.net/pub/firefox/releases/89.0/win64/en-US/Firefox%20Setup%2089.0.exe', 'firefox-installer.exe') }"
-firefox-installer.exe /S
+echo Checking if Firefox is already installed...
+reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Mozilla\Mozilla Firefox" /v DisplayName >nul 2>&1
+if %errorlevel% == 1 (
+  echo Firefox is not installed. Installing...
+  powershell -Command "& { (New-Object Net.WebClient).DownloadFile('https://download-installer.cdn.mozilla.net/pub/firefox/releases/89.0/win64/en-US/Firefox%20Setup%2089.0.exe', 'firefox-installer.exe') }"
+  firefox-installer.exe /S
+) else (
+  echo Firefox is already installed. Skipping installation.
+)
 
 echo Installation complete.
