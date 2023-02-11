16b8cbcfa287a81eec4afa4ecf96b6bae7583b65
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-02-11 22:16:54 +0530

    Bot tst

--- Github/simple-gitconfig.cmd
+++ Github/simple-gitconfig.cmd
@@ -1,29 +1,29 @@
 @echo off
 
-rem Define the email and username for both users
+rem Define the email and username for both users.
 set user1_email=37282143+Sneha@users.noreply.github.com
 set user1_username=Sneha Sharma
 set user2_email=81718060+sakshiagrwal@users.noreply.github.com
 set user2_username=Sakshi Aggarwal
 
-rem Define the GPG signing key for both users
+rem Define the GPG signing key for both users.
 set user1_signingkey=6EB51B3E1C6B549F
 set user2_signingkey=6EB51B3E1C6B549F
 
-rem Define the GPG executable path for both users
+rem Define the GPG executable path for both users.
 set gpg_program=C:/Program Files (x86)/GnuPG/bin/gpg.exe
 
 :start
-rem Prompt the user for the number of the user they want to add
+rem Prompt the user for the number of the user they want to add.
 set /p choice=Enter the number of the user you want to add (1 for %user1_username% or 2 for %user2_username%):
 
-rem Validate the user input
+rem Validate the user input.
 if not "%choice%" == "1" if not "%choice%" == "2" (
     echo Invalid choice. Please enter either 1 or 2.
     goto start
 )
 
-rem Set the email, username, and GPG signing key based on the user's choice
+rem Set the email, username, and GPG signing key based on the user's choice.
 if "%choice%" == "1" (
     set email=%user1_email%
     set username=%user1_username%
@@ -36,7 +36,7 @@ if "%choice%" == "1" (
     set user=%user2_username%
 )
 
-rem Create the .gitconfig file
+rem Create the .gitconfig file.
 echo [user]> %USERPROFILE%\.gitconfig
 echo 	name = %username%>> %USERPROFILE%\.gitconfig
 echo 	email = %email%>> %USERPROFILE%\.gitconfig
@@ -48,12 +48,20 @@ echo 	gpgsign = true>> %USERPROFILE%\.gitconfig
 echo [core]>> %USERPROFILE%\.gitconfig
 echo 	autocrlf = false>> %USERPROFILE%\.gitconfig
 echo 	editor = 'C:/Program Files/Notepad++/notepad++.exe' -multiInst -notabbar -nosession -noPlugin>> %USERPROFILE%\.gitconfig
+echo.
+echo.
+echo Created .gitconfig for %user% in %USERPROFILE%.
+type %USERPROFILE%\.gitconfig
 
-rem Create the .minttyrc file
+rem Create the .minttyrc file.
 echo Columns=120> %USERPROFILE%\.minttyrc
 echo Rows=25>> %USERPROFILE%\.minttyrc
 echo ThemeFile=flat-ui>> %USERPROFILE%\.minttyrc
 echo Font=Cascadia Mono>> %USERPROFILE%\.minttyrc
+echo.
+echo.
+echo Created .minttyrc in %USERPROFILE%.
+type %USERPROFILE%\.minttyrc
 
-echo The .gitconfig and .minttyrc files have been successfully created for %user% in %USERPROFILE%
-pause
+echo Press any key to exit...
+pause > nul
