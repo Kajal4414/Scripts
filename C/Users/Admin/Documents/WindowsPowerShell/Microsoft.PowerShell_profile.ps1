8271634f98b11c758ab32174349f179a76e9e52a
sakshiagrwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-10-24 23:20:55 +0530

    Create Microsoft.PowerShell_profile.ps1

---
+++ C/Users/Admin/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+# oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/jandedobbeleer.omp.json' | Invoke-Expression
+oh-my-posh init pwsh | Invoke-Expression
