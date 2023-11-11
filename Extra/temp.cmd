7ab794f878b23b8ba0fdd30738bd577b6a2fc3e8
sakshiagrwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-11-11 23:34:01 +0530

    ...

--- Extra/temp.cmd
+++ Extra/temp.cmd
@@ -6,64 +6,68 @@ if exist "%ProgramFiles%\ModifiableWindowsApps" (
     icacls "%ProgramFiles%\ModifiableWindowsApps" /grant administrators:F /T
     rmdir /S /Q "%ProgramFiles%\ModifiableWindowsApps"
     echo.
-    pause
 )
 
 if exist "%UserProfile%\AppData\Local\Temp" (
     echo Deleting the "%UserProfile%\AppData\Local\Temp" directory...
     rmdir /S /Q "%UserProfile%\AppData\Local\Temp"
     echo.
-    pause
+)
+
+if exist "%UserProfile%\.vscode\cli" (
+    echo Deleting the "%UserProfile%\.vscode\cli" directory...
+    rmdir /S /Q "%UserProfile%\.vscode\cli"
+    echo.
 )
 
 if exist "%UserProfile%\Recent" (
     echo Deleting the "%UserProfile%\Recent" directory...
     rmdir /S /Q "%UserProfile%\Recent"
     echo.
-    pause
 )
 
 if exist "%UserProfile%\Searches" (
     echo Deleting the "%UserProfile%\Searches" directory...
     rmdir /S /Q "%UserProfile%\Searches"
     echo.
-    pause
 )
 
 if exist "%UserProfile%\Favorites" (
     echo Deleting the "%UserProfile%\Favorites" directory...
     rmdir /S /Q "%UserProfile%\Favorites"
     echo.
-    pause
+)
+
+if exist "%UserProfile%\Saved Games" (
+    echo Deleting the "%UserProfile%\Saved Games" directory...
+    rmdir /S /Q "%UserProfile%\Saved Games"
+    echo.
 )
 
 if exist "%Public%\Desktop" (
     echo Deleting the "%Public%\Desktop" directory...
     rmdir /S /Q "%Public%\Desktop"
     echo.
-    pause
 )
 
 if exist "%WinDir%\AppReadiness" (
     echo Deleting the "%WinDir%\AppReadiness" directory...
     rmdir /S /Q "%WinDir%\AppReadiness"
     echo.
-    pause
 )
 
 if exist "%WinDir%\temp" (
     echo Deleting the "%WinDir%\temp" directory...
     rmdir /S /Q "%WinDir%\temp"
     echo.
-    pause
 )
 
 if exist "%WinDir%\Prefetch" (
     echo Deleting the "%WinDir%\Prefetch" directory...
     rmdir /S /Q "%WinDir%\Prefetch"
     echo.
-    pause
 )
 
 echo The script is finished.
+echo.
 pause
