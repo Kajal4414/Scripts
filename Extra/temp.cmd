d3a9004ae0e38af796e86c991ac514a125969875
sakshiagrwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-11-13 06:22:53 +0000

    updt

--- Extra/temp.cmd
+++ Extra/temp.cmd
@@ -1,73 +1,42 @@
 @echo off
+echo Starting cleanup script...
 
+:: Delete ModifiableWindowsApps directory with takeown and icacls commands
 if exist "%ProgramFiles%\ModifiableWindowsApps" (
     echo Deleting the "%ProgramFiles%\ModifiableWindowsApps" directory...
+    
+    :: Take ownership of the directory
     takeown /F "%ProgramFiles%\ModifiableWindowsApps" /R /D Y >NUL 2>&1
+    
+    :: Grant administrators full control
     icacls "%ProgramFiles%\ModifiableWindowsApps" /grant administrators:F /T >NUL 2>&1
+    
+    :: Remove the directory
     rmdir /S /Q "%ProgramFiles%\ModifiableWindowsApps"
     echo.
 )
 
-if exist "%UserProfile%\AppData\Local\Temp" (
-    echo Deleting the "%UserProfile%\AppData\Local\Temp" directory...
-    rmdir /S /Q "%UserProfile%\AppData\Local\Temp"
-    echo.
+:: Delete other directories
+:: Add more if statements for additional directories as needed
+for %%D in (
+    "%UserProfile%\AppData\Local\Temp"
+    "%UserProfile%\.vscode\cli"
+    "%UserProfile%\Recent"
+    "%UserProfile%\Searches"
+    "%UserProfile%\Favorites"
+    "%UserProfile%\Saved Games"
+    "%Public%\Desktop"
+    "%WinDir%\AppReadiness"
+    "%WinDir%\temp"
+    "%WinDir%\Prefetch"
+) do (
+    if exist %%D (
+        echo Deleting the "%%~D" directory...
+        rmdir /S /Q "%%~D"
+        echo.
+    )
 )
 
-if exist "%UserProfile%\.vscode\cli" (
-    echo Deleting the "%UserProfile%\.vscode\cli" directory...
-    rmdir /S /Q "%UserProfile%\.vscode\cli"
-    echo.
-)
-
-if exist "%UserProfile%\Recent" (
-    echo Deleting the "%UserProfile%\Recent" directory...
-    rmdir /S /Q "%UserProfile%\Recent"
-    echo.
-)
-
-if exist "%UserProfile%\Searches" (
-    echo Deleting the "%UserProfile%\Searches" directory...
-    rmdir /S /Q "%UserProfile%\Searches"
-    echo.
-)
-
-if exist "%UserProfile%\Favorites" (
-    echo Deleting the "%UserProfile%\Favorites" directory...
-    rmdir /S /Q "%UserProfile%\Favorites"
-    echo.
-)
-
-if exist "%UserProfile%\Saved Games" (
-    echo Deleting the "%UserProfile%\Saved Games" directory...
-    rmdir /S /Q "%UserProfile%\Saved Games"
-    echo.
-)
-
-if exist "%Public%\Desktop" (
-    echo Deleting the "%Public%\Desktop" directory...
-    rmdir /S /Q "%Public%\Desktop"
-    echo.
-)
-
-if exist "%WinDir%\AppReadiness" (
-    echo Deleting the "%WinDir%\AppReadiness" directory...
-    rmdir /S /Q "%WinDir%\AppReadiness"
-    echo.
-)
-
-if exist "%WinDir%\temp" (
-    echo Deleting the "%WinDir%\temp" directory...
-    rmdir /S /Q "%WinDir%\temp"
-    echo.
-)
-
-if exist "%WinDir%\Prefetch" (
-    echo Deleting the "%WinDir%\Prefetch" directory...
-    rmdir /S /Q "%WinDir%\Prefetch"
-    echo.
-)
-
-echo The script is finished.
+echo The script has completed successfully.
 echo.
 pause
