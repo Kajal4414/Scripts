a5322175ad6b8a698775058185c199c8cfbd9e2b
sakshiagrwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-11-18 16:27:20 +0530

    Update `TEMP.cmd`
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- Extra/temp.cmd
+++ Extra/temp.cmd
@@ -2,34 +2,31 @@
 
 :: Delete ModifiableWindowsApps directory with 'takeown' and 'icacls' commands.
 if exist "%ProgramFiles%\ModifiableWindowsApps" (
-    whoami /user | find /i "S-1-5-18" >NUL 2>&1 || (
-        call RunAsTI.cmd "%~f0" %*
-        exit /b
-    )
     echo Deleting the "%ProgramFiles%\ModifiableWindowsApps" directory...
-    takeown /F "%ProgramFiles%\ModifiableWindowsApps" /R /D Y >NUL 2>&1
-    icacls "%ProgramFiles%\ModifiableWindowsApps" /grant administrators:F /T >NUL 2>&1
+    takeown /F "%ProgramFiles%\ModifiableWindowsApps" /A >NUL 2>&1 & icacls "%ProgramFiles%\ModifiableWindowsApps" /grant Administrators:F >NUL 2>&1
     RD /S /Q "%ProgramFiles%\ModifiableWindowsApps"
     echo.
 )
 
-:: Delete other directories ($ rmdir "C:\Windows\temp\*").
+:: Delete other directories.
 for %%D in (
-    "%UserProfile%\AppData\Local\Temp"
+    "%UserProfile%\.android"
+    "%UserProfile%\.dbus-keyrings"
     "%UserProfile%\.vscode\cli"
+    "%UserProfile%\AppData\Local\Temp"
+    "%UserProfile%\Favorites"
     "%UserProfile%\Recent"
     "%UserProfile%\Searches"
-    "%UserProfile%\Favorites"
     "%UserProfile%\Saved Games"
+    "%UserProfile%\Videos\Captures"
     "%Public%\Desktop"
     "%WinDir%\AppReadiness"
     "%WinDir%\Prefetch"
-) do (
-    :: Add more if statements for additional directories as needed.
+    "%WinDir%\temp"
+    ) do (
     if exist "%%~D" (
         echo Deleting the "%%~D" directory...
         RD /S /Q "%%~D" 2>NUL
-        echo.
         echo.
     )
 )
