d125b2288a903984ae0854832bd71ed19cb102cf
sakshiagrwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-11-22 22:24:39 +0000

    windows: cleaner: Update
    Signed-off-by: sakshiagrwal <81718060+sakshiagrwal@users.noreply.github.com>

--- Windows/Extra/cleaner.cmd
+++ Windows/Extra/cleaner.cmd
@@ -1,35 +1,48 @@
 @echo off
 
-:: Delete ModifiableWindowsApps directory with 'takeown' and 'icacls' commands.
-if exist "%ProgramFiles%\ModifiableWindowsApps" (
-    echo Deleting the "%ProgramFiles%\ModifiableWindowsApps" directory...
-    takeown /F "%ProgramFiles%\ModifiableWindowsApps" /A >NUL 2>&1 & icacls "%ProgramFiles%\ModifiableWindowsApps" /grant Administrators:F >NUL 2>&1
-    RD /S /Q "%ProgramFiles%\ModifiableWindowsApps"
-    echo.
+:: Delete directories requiring 'takeown' and 'icacls' commands.
+for %%D in (
+    "%ProgramFiles%\ModifiableWindowsApps"
+    "%Public%\Desktop"
+    "%WinDir%\SoftwareDistribution\Download"
+    "%WinDir%\SoftwareDistribution\PostRebootEventCache.V2"
+    "%WinDir%\temp"
+    ) do (
+    if exist "%%~D" (
+        echo Deleting the "%%~D" directory...
+        takeown /F "%%~D" /A >NUL 2>&1 & icacls "%%~D" /grant Administrators:F >NUL 2>&1
+        RD /S /Q "%%~D"
+    )
 )
 
 :: Delete other directories.
 for %%D in (
+    "%AppData%\DMCache"
     "%UserProfile%\.android"
     "%UserProfile%\.dbus-keyrings"
     "%UserProfile%\.vscode\cli"
     "%UserProfile%\AppData\Local\Temp"
+    "%UserProfile%\AppData\Local\D3DSCache"
+    "%UserProfile%\AppData\Local\npm-cache"
+    "%UserProfile%\AppData\Local\PeerDistRepub"
+    "%UserProfile%\AppData\Local\pylint"
+    "%UserProfile%\AppData\Local\VirtualStore"
+    "%UserProfile%\AppData\LocalLow\AMD"
+    "%UserProfile%\AppData\Roaming\Adobe"
     "%UserProfile%\Favorites"
     "%UserProfile%\Recent"
     "%UserProfile%\Searches"
     "%UserProfile%\Saved Games"
     "%UserProfile%\Videos\Captures"
-    "%Public%\Desktop"
     "%WinDir%\AppReadiness"
     "%WinDir%\Prefetch"
-    "%WinDir%\temp"
     ) do (
     if exist "%%~D" (
         echo Deleting the "%%~D" directory...
         RD /S /Q "%%~D" 2>NUL
-        echo.
     )
 )
 
+echo.
 echo Press any key to exit...
 pause >NUL
