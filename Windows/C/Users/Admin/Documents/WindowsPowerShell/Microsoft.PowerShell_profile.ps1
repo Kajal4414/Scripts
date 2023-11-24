6fdef283c845f6497885e273a736f2bc0274b78e
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-11-24 08:29:18 +0530

    code: settings: Update - catppuccin settings: https://github.com/catppuccin/vscode#catppuccin-settings - use mocha omp theme for powershell
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- Windows/C/Users/Admin/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/C/Users/Admin/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
 # https://ohmyposh.dev/docs/installation/customize
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/1_shell.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/catppuccin_mocha.omp.json" | Invoke-Expression
