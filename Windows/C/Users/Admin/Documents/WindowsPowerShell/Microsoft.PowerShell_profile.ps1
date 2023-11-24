8c390b5d482e43731800044dcf1b905dbd683b23
sakshiagrwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-11-24 14:48:54 +0530

    windows: powershell: Update setting

--- Windows/C/Users/Admin/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/C/Users/Admin/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
 # https://ohmyposh.dev/docs/installation/customize
+Clear-Host # clear copyright banner
+Set-Location "$env:USERPROFILE\Desktop" # set default working directory
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/catppuccin_mocha.omp.json" | Invoke-Expression
