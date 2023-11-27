64dbee53eac088d09bf6b684ed55fec7f60dbbf1
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-11-27 07:00:06 +0530

    windows: cleaner: Update cleaner.ps1
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- Windows/Extra/cleaner.ps1
+++ Windows/Extra/cleaner.ps1
@@ -1,51 +1,52 @@
-@echo off
-
-:: Delete directories requiring 'takeown' and 'icacls' commands.
-for %%D in (
-    "%ProgramFiles%\ModifiableWindowsApps"
-    "%Public%\Desktop"
-    "%WinDir%\SoftwareDistribution\Download"
-    "%WinDir%\SoftwareDistribution\PostRebootEventCache.V2"
-    "%WinDir%\temp"
-    ) do (
-    if exist "%%~D" (
-        echo Deleting the "%%~D" directory...
-        takeown /F "%%~D" /A >NUL 2>&1 & icacls "%%~D" /grant Administrators:F >NUL 2>&1
-        RD /S /Q "%%~D"
-    )
+# Delete directories requiring 'takeown' and 'icacls' commands
+$directories = @(
+    "$Env:ProgramFiles\ModifiableWindowsApps",
+    "$Env:Public\Desktop",
+    "$Env:WinDir\SoftwareDistribution\Download",
+    "$Env:WinDir\SoftwareDistribution\PostRebootEventCache.V2",
+    "$Env:WinDir\temp"
 )
 
-:: Delete other directories.
-for %%D in (
-    "%AppData%\DMCache"
-    "%UserProfile%\.android"
-    "%UserProfile%\.dbus-keyrings"
-    "%UserProfile%\.vscode\cli"
-    "%UserProfile%\AppData\Local\Temp"
-    "%UserProfile%\AppData\Local\D3DSCache"
-    "%UserProfile%\AppData\Local\fontconfig"
-    "%UserProfile%\AppData\Local\npm-cache"
-    "%UserProfile%\AppData\Local\PeerDistRepub"
-    "%UserProfile%\AppData\Local\pip"
-    "%UserProfile%\AppData\Local\pylint"
-    "%UserProfile%\AppData\Local\VirtualStore"
-    "%UserProfile%\AppData\LocalLow\AMD"
-    "%UserProfile%\AppData\Roaming\Adobe"
-    "%UserProfile%\Favorites"
-    "%UserProfile%\Recent"
-    "%UserProfile%\Searches"
-    "%UserProfile%\Saved Games"
-    "%UserProfile%\Videos\Captures"
-    "%WinDir%\AppReadiness"
-    "%WinDir%\Prefetch"
-    ) do (
-    if exist "%%~D" (
-        echo Deleting the "%%~D" directory...
-        RD /S /Q "%%~D" 2>NUL
-    )
+foreach ($dir in $directories) {
+    if (Test-Path $dir) {
+        Write-Host "Deleting the $dir directory..."
+        Take-Ownership -Path $dir | Out-Null
+        icacls $dir /grant Administrators:F | Out-Null
+        Remove-Item $dir -Recurse -Force
+    }
+}
+
+# Delete other directories
+$otherDirectories = @(
+    "$Env:AppData\DMCache",
+    "$Env:UserProfile\.android",
+    "$Env:UserProfile\.dbus-keyrings",
+    "$Env:UserProfile\.vscode\cli",
+    "$Env:UserProfile\AppData\Local\Temp",
+    "$Env:UserProfile\AppData\Local\D3DSCache",
+    "$Env:UserProfile\AppData\Local\fontconfig",
+    "$Env:UserProfile\AppData\Local\npm-cache",
+    "$Env:UserProfile\AppData\Local\PeerDistRepub",
+    "$Env:UserProfile\AppData\Local\pip",
+    "$Env:UserProfile\AppData\Local\pylint",
+    "$Env:UserProfile\AppData\Local\VirtualStore",
+    "$Env:UserProfile\AppData\LocalLow\AMD",
+    "$Env:UserProfile\AppData\Roaming\Adobe",
+    "$Env:UserProfile\Favorites",
+    "$Env:UserProfile\Recent",
+    "$Env:UserProfile\Searches",
+    "$Env:UserProfile\Saved Games",
+    "$Env:UserProfile\Videos\Captures",
+    "$Env:WinDir\AppReadiness",
+    "$Env:WinDir\Prefetch"
 )
 
-echo.
-echo Exiting in 3 seconds...
-timeout /t 3 >NUL
-exit
+foreach ($otherDir in $otherDirectories) {
+    if (Test-Path $otherDir) {
+        Write-Host "Deleting the $otherDir directory..."
+        Remove-Item $otherDir -Recurse -Force -ErrorAction SilentlyContinue
+    }
+}
+
+Write-Host "Exiting in 3 seconds..."
+Start-Sleep -Seconds 3
