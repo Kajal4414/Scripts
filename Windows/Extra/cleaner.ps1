3d460e1df0bc84a5128181b68397f81cc87d1f46
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-11-27 07:07:18 +0530

    cleaner.ps1: WIP
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- Windows/Extra/cleaner.ps1
+++ Windows/Extra/cleaner.ps1
@@ -1,4 +1,48 @@
-# Delete directories requiring 'takeown' and 'icacls' commands
+# Function to invoke Disk Cleanup based on predefined registry settings
+function Invoke-DiskCleanup {
+    # Stop existing Disk Cleanup process if running
+    Get-Process -Name cleanmgr -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
+
+    # Define registry settings
+    $baseKey = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches'
+    $regValues = @{
+        "Active Setup Temp Folders"             = 2
+        "BranchCache"                           = 2
+        "D3D Shader Cache"                      = 0
+        "Delivery Optimization Files"           = 2
+        "Diagnostic Data Viewer database files" = 2
+        "Downloaded Program Files"              = 2
+        "Internet Cache Files"                  = 2
+        "Language Pack"                         = 0
+        "Old ChkDsk Files"                      = 2
+        "Recycle Bin"                           = 0
+        "RetailDemo Offline Content"            = 2
+        "Setup Log Files"                       = 2
+        "System error memory dump files"        = 2
+        "System error minidump files"           = 2
+        "Temporary Files"                       = 0
+        "Thumbnail Cache"                       = 2
+        "Update Cleanup"                        = 2
+        "User file versions"                    = 2
+        "Windows Error Reporting Files"         = 2
+        "Windows Defender"                      = 2
+        "Temporary Sync Files"                  = 2
+        "Device Driver Packages"                = 2
+    }
+
+    # Set registry settings for Disk Cleanup
+    foreach ($entry in $regValues.GetEnumerator()) {
+        Set-ItemProperty -Path "$baseKey\$($entry.Key)" -Name 'StateFlags0064' -Value $entry.Value -Type DWORD
+    }
+
+    # Start Disk Cleanup with predefined settings
+    Start-Process -FilePath "cleanmgr.exe" -ArgumentList "/sagerun:64" -Wait
+}
+
+# Invoke Disk Cleanup
+Invoke-DiskCleanup
+
+# Delete directories with administrator privileges
 $directories = @(
     "$Env:ProgramFiles\ModifiableWindowsApps",
     "$Env:Public\Desktop",
@@ -48,5 +92,6 @@ foreach ($otherDir in $otherDirectories) {
     }
 }
 
+Write-Host ""
 Write-Host "Exiting in 3 seconds..."
 Start-Sleep -Seconds 3
