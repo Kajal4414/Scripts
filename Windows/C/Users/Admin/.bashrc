60383cfbc93ade4cabb95e1564f5f58d767337fd
sakshiagrwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-11-29 05:47:32 +0530

    windows: Update `VSCode` settings & some themes
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- Windows/C/Users/Admin/.bashrc
+++ Windows/C/Users/Admin/.bashrc
@@ -1,3 +1,3 @@
 # https://ohmyposh.dev/docs/installation/customize
 # eval "$(oh-my-posh init bash --config "$USERPROFILE\AppData\Local\Programs\oh-my-posh\themes\jandedobbeleer.omp.json")"
-eval "$(oh-my-posh init bash --config "$POSH_THEMES_PATH\1_shell.omp.json")"
+eval "$(oh-my-posh init bash --config "$POSH_THEMES_PATH\tiwahu.omp.json")"
--- Windows/C/Users/Admin/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/C/Users/Admin/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
 # https://ohmyposh.dev/docs/installation/customize
 Clear-Host # clear copyright banner
-Set-Location "$env:USERPROFILE\Desktop" # set default working directory
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/catppuccin_mocha.omp.json" | Invoke-Expression
+# Set-Location "$env:USERPROFILE\Desktop" # set default working directory
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tiwahu.omp.json" | Invoke-Expression
