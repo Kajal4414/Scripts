09770882add53a84ff2a15f0a1ae759baa0bc92a
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-12-03 22:09:33 +0530

    windows: install_apps.ps1: Okay... [1] 1695b12. [2] 401f5f6cac065f2ed0ad3071234f1532eac6ff7c.
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- Windows/Software/install_apps.ps1
+++ Windows/Software/install_apps.ps1
@@ -1,49 +1,58 @@
-# Define software URLs
+# Define software URLs with splatting for better readability
 $softwareURLs = @{
-    "7-Zip v23.01 x64"                 = "https://www.7-zip.org/a/7z2301-x64.exe"
-    "BleachBit v4.6.0 x64"             = "https://download.bleachbit.org/BleachBit-4.6.0-setup.exe"
-    "Chrome vLatest x64"               = "https://dl.google.com/chrome/install/ChromeStandaloneSetup64.exe"
-    "Git v2.43.0 x64"                  = "https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe"
-    "IDM v6.42 Build 2 x86"            = "https://mirror2.internetdownloadmanager.com/idman642build2.exe?v=lt&filename=idman642build2.exe"
-    "IntelliJ IDEA v2023.2.5 x64"      = "https://download-cdn.jetbrains.com/idea/ideaIC-2023.2.5.exe"
-    "JDK v21.0.1 x64"                  = "https://download.oracle.com/java/21/latest/jdk-21_windows-x64_bin.exe"
-    "K-Lite Codec Pack v17.9.4 x64"    = "https://files2.codecguide.com/K-Lite_Codec_Pack_1794_Full.exe"
-    "Notepad++ v8.6 x64"               = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.6/npp.8.6.Installer.x64.exe"
-    "ProtonVPN v3.2.7 x64"             = "https://protonvpn.com/download/ProtonVPN_v3.2.7.exe"
-    "Node.js v20.10.0 x64"             = "https://nodejs.org/dist/v20.10.0/node-v20.10.0-x64.msi"
-    "PyCharm v2023.2.5 x64"            = "https://download-cdn.jetbrains.com/python/pycharm-community-2023.2.5.exe"
-    "Python v3.12.0 x64"               = "https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe"
-    "Revo Uninstaller Pro vLatest x64" = "https://download.revouninstaller.com/download/RevoUninProSetup.exe"
-    "StartIsBack vLatest x86"          = "https://startisback.sfo3.cdn.digitaloceanspaces.com/StartIsBackPlusPlus_setup.exe"
-    "Telegram vLatest x64"             = "https://telegram.org/dl/desktop/win64"
-    "Visual Studio Code v1.84.2 x64"   = "https://az764295.vo.msecnd.net/stable/1a5daa3a0231a0fbba4f14db7ec463cf99d7768e/VSCodeSetup-x64-1.84.2.exe"
-    "VLC v3.0.20 x64"                  = "https://mirror.kku.ac.th/videolan/vlc/3.0.20/win64/vlc-3.0.20-win64.exe"
-    "Youtube Downloader v1.10.8 x64"   = "https://github.com/Tyrrrz/YoutubeDownloader/releases/download/1.10.8/YoutubeDownloader.zip"
+    "7-Zip v23.01 x64.exe"                 = "https://www.7-zip.org/a/7z2301-x64.exe"
+    "BleachBit v4.6.0 x64.exe"             = "https://download.bleachbit.org/BleachBit-4.6.0-setup.exe"
+    "Chrome vLatest x64.exe"               = "https://dl.google.com/chrome/install/ChromeStandaloneSetup64.exe"
+    "Git v2.43.0 x64.exe"                  = "https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe"
+    "IDM v6.42 Build 2 x86.exe"            = "https://mirror2.internetdownloadmanager.com/idman642build2.exe?v=lt&filename=idman642build2.exe"
+    "IntelliJ IDEA v2023.2.5 x64.exe"      = "https://download-cdn.jetbrains.com/idea/ideaIC-2023.2.5.exe"
+    "JDK v21.0.1 x64.exe"                  = "https://download.oracle.com/java/21/latest/jdk-21_windows-x64_bin.exe"
+    "K-Lite Codec Pack v17.9.4 x64.exe"    = "https://files2.codecguide.com/K-Lite_Codec_Pack_1794_Full.exe"
+    "Notepad++ v8.6 x64.exe"               = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.6/npp.8.6.Installer.x64.exe"
+    "ProtonVPN v3.2.7 x64.exe"             = "https://protonvpn.com/download/ProtonVPN_v3.2.7.exe"
+    "Node.js v20.10.0 x64.exe"             = "https://nodejs.org/dist/v20.10.0/node-v20.10.0-x64.msi"
+    "PyCharm v2023.2.5 x64.exe"            = "https://download-cdn.jetbrains.com/python/pycharm-community-2023.2.5.exe"
+    "Python v3.12.0 x64.exe"               = "https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe"
+    "Revo Uninstaller Pro vLatest x64.exe" = "https://download.revouninstaller.com/download/RevoUninProSetup.exe"
+    "StartIsBack vLatest x86.exe"          = "https://startisback.sfo3.cdn.digitaloceanspaces.com/StartIsBackPlusPlus_setup.exe"
+    "Telegram vLatest x64.exe"             = "https://telegram.org/dl/desktop/win64"
+    "Visual Studio Code v1.84.2 x64.exe"   = "https://az764295.vo.msecnd.net/stable/1a5daa3a0231a0fbba4f14db7ec463cf99d7768e/VSCodeSetup-x64-1.84.2.exe"
+    "VLC v3.0.20 x64.exe"                  = "https://mirror.kku.ac.th/videolan/vlc/3.0.20/win64/vlc-3.0.20-win64.exe"
+    "Youtube Downloader v1.10.8 x64.zip"   = "https://github.com/Tyrrrz/YoutubeDownloader/releases/download/1.10.8/YoutubeDownloader.zip"
 }
 
 # Define download folder
 $downloadFolder = "$Env:UserProfile\Downloads"
 
+# Function to check admin privileges
+function TestAdmin {
+    $currentPrincipal = [Security.Principal.WindowsPrincipal]::new([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
 # Function to download software
 function DownloadSoftware {
     param($appName, $appURL)
 
-    $filePath = Join-Path -Path $downloadFolder -ChildPath "$appName.*"
+    $filePath = Join-Path -Path $downloadFolder -ChildPath $appName
 
     if (Test-Path -Path $filePath) {
         Write-Host "Skipped: '$appName' already exists." -ForegroundColor Yellow
         return
     }
 
-    $fileName = Join-Path -Path $downloadFolder -ChildPath "$appName.exe"
+    if (-not (TestAdmin)) {
+        Write-Host "Error: Administrator privileges required." -ForegroundColor Red
+        exit
+    }
 
     try {
-        Write-Host "Downloading '$appName'..." -ForegroundColor Cyan
+        Write-Host "Downloading '$appName'" -ForegroundColor Cyan
 
         $webRequest = [System.Net.WebRequest]::Create($appURL)
         $response = $webRequest.GetResponse()
         $stream = $response.GetResponseStream()
-        $fileStream = [System.IO.File]::Create($fileName)
+        $fileStream = [System.IO.File]::Create($filePath)
 
         $bufferSize = 8192
         $buffer = New-Object Byte[] $bufferSize
@@ -60,7 +69,7 @@ function DownloadSoftware {
             $percentComplete = ($bytesRead / $response.ContentLength) * 100
 
             $status = "Downloaded {0:F2} MB of {1:F2} MB" -f $megabytesDownloaded, $totalMegabytes
-            Write-Progress -Activity "Downloading $appName" -Status $status -PercentComplete $percentComplete
+            Write-Progress -Activity "Downloading '$appName'" -Status $status -PercentComplete $percentComplete
         } while ($read -gt 0)
 
         $fileStream.Close()
@@ -68,15 +77,15 @@ function DownloadSoftware {
         $response.Close()
     }
     catch {
-        Write-Host "Error downloading '$appName' $_" -ForegroundColor Red
+        Write-Host "Error: downloading '$appName' $_" -ForegroundColor Red
     }
     finally {
-        if ($fileStream) { $fileStream.Close() }
-        if ($stream) { $stream.Close() }
-        if ($response) { $response.Close() }
-        if ($fileStream) { $fileStream.Dispose() }
-        if ($stream) { $stream.Dispose() }
-        if ($response) { $response.Dispose() }
+        $fileStream, $stream, $response | ForEach-Object {
+            if ($_ -ne $null) {
+                $_.Close()
+                $_.Dispose()
+            }
+        }
     }
 }
 
@@ -85,20 +94,18 @@ function InstallSoftware {
     param($appName)
 
     if ($appName -like "Youtube Downloader*") {
-        Write-Host "Preparing installer for '$appName.zip'..." -ForegroundColor Cyan
-        Rename-Item -Path "$downloadFolder\$appName.exe" -NewName "$downloadFolder\$appName.zip"
-        Write-Host "Extracting '$appName.zip' installer to C:\Program Files\YoutubeDownloader..." -ForegroundColor Cyan
-        Expand-Archive -Path "$downloadFolder\$appName.zip" -DestinationPath "C:\Program Files\YoutubeDownloader"
+        Write-Host "Extracting '$appName' installer to C:\Program Files\YoutubeDownloader..." -ForegroundColor Cyan
+        Expand-Archive -Path "$downloadFolder\$appName" -DestinationPath "C:\Program Files\YoutubeDownloader" -Force
         return
     }
 
-    $installerPath = Join-Path -Path $downloadFolder -ChildPath "$appName.exe"
+    $installerPath = Join-Path -Path $downloadFolder -ChildPath "$appName"
     if (-not (Test-Path -Path $installerPath)) {
         Write-Host "Skipped: '$appName' installer not found." -ForegroundColor Yellow
         return
     }
 
-    Write-Host "Installing '$appName.exe'..." -ForegroundColor Cyan
+    Write-Host "Installing '$appName'" -ForegroundColor Cyan
     Start-Process -FilePath $installerPath -ArgumentList "/S" -Wait
 }
 
@@ -114,7 +121,7 @@ $startIsBackDirectory = "C:\Program Files (x86)\StartIsBack"
 $vsCodeSettingsDirectory = "$Env:UserProfile\AppData\Roaming\Code\User"
 $revoUninstallerDirectory = "C:\ProgramData\VS Revo Group\Revo Uninstaller Pro"
 
-# Function to ask for input with default value and re-ask for incorrect input
+# Function to prompt for input with default value
 function PromptForInputWithDefault($message, $defaultValue) {
     $userInput = ""
     while ($userInput -ne "y" -and $userInput -ne "n") {
@@ -129,7 +136,7 @@ function PromptForInputWithDefault($message, $defaultValue) {
     return $userInput
 }
 
-# Configure Visual Studio Code settings if directory exists
+# Configuring VS Code settings if directory exists
 if (Test-Path -Path $vsCodeSettingsDirectory -PathType Container) {
     $configureVSCode = PromptForInputWithDefault "Do you want to configure Visual Studio Code settings?" "n"
     if ($configureVSCode -eq "y") {
@@ -138,7 +145,7 @@ if (Test-Path -Path $vsCodeSettingsDirectory -PathType Container) {
     }
 }
 
-# Activate Revo Uninstaller Pro if directory exists
+# Activating Revo Uninstaller Pro if directory exists
 if (Test-Path -Path $revoUninstallerDirectory -PathType Container) {
     $activateRevoUninstaller = PromptForInputWithDefault "Do you want to activate Revo Uninstaller Pro?" "n"
     if ($activateRevoUninstaller -eq "y") {
@@ -147,7 +154,7 @@ if (Test-Path -Path $revoUninstallerDirectory -PathType Container) {
     }
 }
 
-# Activate StartIsBack++ if directory exists
+# Activating StartIsBack++ if directory exists
 if (Test-Path -Path $startIsBackDirectory -PathType Container) {
     $activateStartIsBack = PromptForInputWithDefault "Do you want to activate StartIsBack++?" "n"
     if ($activateStartIsBack -eq "y") {
@@ -156,7 +163,7 @@ if (Test-Path -Path $startIsBackDirectory -PathType Container) {
     }
 }
 
-# Activate Internet Download Manager if directory exists
+# Activating Internet Download Manager if directory exists
 if (Test-Path -Path $idmDirectory -PathType Container) {
     $activateIDM = PromptForInputWithDefault "Do you want to activate Internet Download Manager?" "n"
     if ($activateIDM -eq "y") {
