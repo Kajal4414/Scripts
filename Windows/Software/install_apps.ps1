785ce6687e72efe79e3126be5bf931407b19cd13
sakshiagrwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-12-04 10:05:41 +0530

    windows: install_apps.ps1: `Comments` And add a function to pause and wait for user input Update some Write-Host
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- Windows/Software/install_apps.ps1
+++ Windows/Software/install_apps.ps1
@@ -24,46 +24,61 @@ $softwareURLs = @{
 # Define download folder
 $downloadFolder = "$Env:UserProfile\Downloads"
 
+# Function to pause and wait for user input
+function PauseNull {
+    Write-Host "Press any key to exit... " -NoNewline
+    $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown') | Out-Null
+    exit
+}
+
 # Function to check admin privileges
 function TestAdmin {
     $currentPrincipal = [Security.Principal.WindowsPrincipal]::new([Security.Principal.WindowsIdentity]::GetCurrent())
     $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
+# Check for admin privileges
+if (-not (TestAdmin)) {
+    Write-Host "Error: Admin privileges required" -ForegroundColor Red
+    PauseNull
+}
+
 # Function to download software
 function DownloadSoftware {
     param($appName, $appURL)
 
+    # Define the file path based on the download folder and software name
     $filePath = Join-Path -Path $downloadFolder -ChildPath $appName
 
+    # Check if the file already exists at the specified file path
     if (Test-Path -Path $filePath) {
-        Write-Host "Skipped: '$appName' already exists." -ForegroundColor Yellow
+        Write-Host "Skipping: '$appName' already exists in the download folder." -ForegroundColor Yellow
         return
     }
 
-    if (-not (TestAdmin)) {
-        Write-Host "Error: Administrator privileges required." -ForegroundColor Red
-        exit
-    }
-
+    # Attempt to download the software
     try {
-        Write-Host "Downloading '$appName'" -ForegroundColor Cyan
+        Write-Host "Downloading '$appName'..." -ForegroundColor Cyan
 
+        # Create a web request to the provided software URL
         $webRequest = [System.Net.WebRequest]::Create($appURL)
         $response = $webRequest.GetResponse()
         $stream = $response.GetResponseStream()
         $fileStream = [System.IO.File]::Create($filePath)
 
+        # Define buffer settings for the download process
         $bufferSize = 8192
         $buffer = New-Object Byte[] $bufferSize
         $bytesInMegabyte = 1MB
         $bytesRead = 0
 
+        # Start downloading in chunks until the entire file is downloaded
         do {
             $read = $stream.Read($buffer, 0, $buffer.Length)
             $fileStream.Write($buffer, 0, $read)
             $bytesRead += $read
 
+            # Calculate download progress and display it
             $megabytesDownloaded = $bytesRead / $bytesInMegabyte
             $totalMegabytes = $response.ContentLength / $bytesInMegabyte
             $percentComplete = ($bytesRead / $response.ContentLength) * 100
@@ -72,14 +87,18 @@ function DownloadSoftware {
             Write-Progress -Activity "Downloading '$appName'" -Status $status -PercentComplete $percentComplete
         } while ($read -gt 0)
 
+        # Close streams and response after download completion
         $fileStream.Close()
         $stream.Close()
         $response.Close()
+
+        Write-Host "Downloaded '$appName' successfully." -ForegroundColor Green
     }
     catch {
-        Write-Host "Error: downloading '$appName' $_" -ForegroundColor Red
+        Write-Host "Error occurred while downloading '$appName': $_" -ForegroundColor Red
     }
     finally {
+        # Dispose resources in the finally block to ensure cleanup
         $fileStream, $stream, $response | ForEach-Object {
             if ($_ -ne $null) {
                 $_.Close()
@@ -125,9 +144,9 @@ $revoUninstallerDirectory = "C:\ProgramData\VS Revo Group\Revo Uninstaller Pro"
 function PromptForInputWithDefault($message, $defaultValue) {
     $userInput = ""
     while ($userInput -ne "y" -and $userInput -ne "n") {
-        $userInput = Read-Host "$message (y/n, default: '$defaultValue')"
+        $userInput = Read-Host "$message (Y/N, Default: $defaultValue)"
         if ($userInput -ne "y" -and $userInput -ne "n" -and $userInput -ne "") {
-            Write-Host "Invalid input. Please enter 'y' or 'n'." -ForegroundColor Red
+            Write-Host "Invalid input. Please enter 'Y' to confirm or 'N' to cancel. (Default: $defaultValue)" -ForegroundColor Red
         }
         elseif ($userInput -eq "") {
             $userInput = $defaultValue
@@ -138,9 +157,11 @@ function PromptForInputWithDefault($message, $defaultValue) {
 
 # Configuring VS Code settings if directory exists
 if (Test-Path -Path $vsCodeSettingsDirectory -PathType Container) {
-    $configureVSCode = PromptForInputWithDefault "Do you want to configure Visual Studio Code settings?" "n"
+    $configureVSCode = PromptForInputWithDefault "Do you want to configure Visual Studio Code settings and install extensions?" "N"
     if ($configureVSCode -eq "y") {
-        Write-Host "Installing extensions..." -ForegroundColor Yellow
+        Write-Host "Installing extensions for Visual Studio Code..." -ForegroundColor Yellow
+
+        # List of VS Code extensions to install
         $extensions = @(
             "Catppuccin.catppuccin-vsc",
             "Catppuccin.catppuccin-vsc-icons",
@@ -169,40 +190,52 @@ if (Test-Path -Path $vsCodeSettingsDirectory -PathType Container) {
             "vscjava.vscode-java-test",
             "vscjava.vscode-maven"
         )
+
+        # Loop through each extension and install it
         foreach ($extension in $extensions) {
+            # Install VS Code extension
             code --install-extension $extension
         }
 
         Write-Host "Configuring Visual Studio Code settings..." -ForegroundColor Cyan
+
+        # Downloading settings file for VS Code
         Invoke-WebRequest -Uri "https://github.com/sakshiagrwal/Scripts/raw/main/Windows/C/Users/Admin/AppData/Roaming/Code/User/settings.json" -OutFile "$vsCodeSettingsDirectory\settings.json"
     }
 }
 
 # Activating Revo Uninstaller Pro if directory exists
 if (Test-Path -Path $revoUninstallerDirectory -PathType Container) {
-    $activateRevoUninstaller = PromptForInputWithDefault "Do you want to activate Revo Uninstaller Pro?" "n"
+    $activateRevoUninstaller = PromptForInputWithDefault "Do you want to activate Revo Uninstaller Pro?" "N"
     if ($activateRevoUninstaller -eq "y") {
         Write-Host "Activating Revo Uninstaller Pro..." -ForegroundColor Cyan
+
+        # Downloading license file for Revo Uninstaller Pro
         Invoke-WebRequest -Uri "https://github.com/sakshiagrwal/Scripts/raw/main/Windows/Extra/revouninstallerpro5.lic" -OutFile "$revoUninstallerDirectory\revouninstallerpro5.lic"
     }
 }
 
 # Activating StartIsBack++ if directory exists
 if (Test-Path -Path $startIsBackDirectory -PathType Container) {
-    $activateStartIsBack = PromptForInputWithDefault "Do you want to activate StartIsBack++?" "n"
+    $activateStartIsBack = PromptForInputWithDefault "Do you want to activate StartIsBack++?" "N"
     if ($activateStartIsBack -eq "y") {
         Write-Host "Activating StartIsBack++..." -ForegroundColor Cyan
+
+        # Downloading file to activate StartIsBack++
         Invoke-WebRequest -Uri "https://github.com/sakshiagrwal/Scripts/raw/main/Windows/Extra/msimg32.dll" -OutFile "$startIsBackDirectory\msimg32.dll"
     }
 }
 
 # Activating Internet Download Manager if directory exists
 if (Test-Path -Path $idmDirectory -PathType Container) {
-    $activateIDM = PromptForInputWithDefault "Do you want to activate Internet Download Manager?" "n"
+    $activateIDM = PromptForInputWithDefault "Do you want to activate Internet Download Manager?" "N"
     if ($activateIDM -eq "y") {
         Write-Host "Activating Internet Download Manager..." -ForegroundColor Cyan
+
+        # Activating IDM through a web request
         Invoke-RestMethod -Uri "https://massgrave.dev/ias" | Invoke-Expression
     }
 }
 
-Write-Host "`nSetup completed." -ForegroundColor Green
+Write-Host "`nSetup completed successfully." -ForegroundColor Green
+PauseNull
