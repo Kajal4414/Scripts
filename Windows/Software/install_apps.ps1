3883816ce880bed96701198d73117a179d084867
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-12-05 11:11:26 +0530

    json
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- Windows/Software/install_apps.ps1
+++ Windows/Software/install_apps.ps1
@@ -1,25 +1,5 @@
-# Define software URLs with splatting for better readability
-$softwareURLs = @{
-    "7-Zip v23.01 x64.exe"                 = "https://www.7-zip.org/a/7z2301-x64.exe"
-    "BleachBit v4.6.0 x64.exe"             = "https://download.bleachbit.org/BleachBit-4.6.0-setup.exe"
-    "Chrome vLatest x64.exe"               = "https://dl.google.com/chrome/install/ChromeStandaloneSetup64.exe"
-    "Git v2.43.0 x64.exe"                  = "https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe"
-    "IDM v6.42 Build 2 x86.exe"            = "https://mirror2.internetdownloadmanager.com/idman642build2.exe?v=lt&filename=idman642build2.exe"
-    "IntelliJ IDEA v2023.2.5 x64.exe"      = "https://download-cdn.jetbrains.com/idea/ideaIC-2023.2.5.exe"
-    "JDK v21.0.1 x64.exe"                  = "https://download.oracle.com/java/21/latest/jdk-21_windows-x64_bin.exe"
-    "K-Lite Codec Pack v17.9.4 x64.exe"    = "https://files2.codecguide.com/K-Lite_Codec_Pack_1794_Full.exe"
-    "Notepad++ v8.6 x64.exe"               = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.6/npp.8.6.Installer.x64.exe"
-    "ProtonVPN v3.2.7 x64.exe"             = "https://protonvpn.com/download/ProtonVPN_v3.2.7.exe"
-    "Node.js v20.10.0 x64.exe"             = "https://nodejs.org/dist/v20.10.0/node-v20.10.0-x64.msi"
-    "PyCharm v2023.2.5 x64.exe"            = "https://download-cdn.jetbrains.com/python/pycharm-community-2023.2.5.exe"
-    "Python v3.12.0 x64.exe"               = "https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe"
-    "Revo Uninstaller Pro vLatest x64.exe" = "https://download.revouninstaller.com/download/RevoUninProSetup.exe"
-    "StartIsBack vLatest x86.exe"          = "https://startisback.sfo3.cdn.digitaloceanspaces.com/StartIsBackPlusPlus_setup.exe"
-    "Telegram vLatest x64.exe"             = "https://telegram.org/dl/desktop/win64"
-    "Visual Studio Code v1.84.2 x64.exe"   = "https://az764295.vo.msecnd.net/stable/1a5daa3a0231a0fbba4f14db7ec463cf99d7768e/VSCodeSetup-x64-1.84.2.exe"
-    "VLC v3.0.20 x64.exe"                  = "https://mirror.kku.ac.th/videolan/vlc/3.0.20/win64/vlc-3.0.20-win64.exe"
-    "Youtube Downloader v1.10.8 x64.zip"   = "https://github.com/Tyrrrz/YoutubeDownloader/releases/download/1.10.8/YoutubeDownloader.zip"
-}
+# Read software URLs from the JSON file
+$softwareURLs = Get-Content -Path ".\Windows\Software\softwareURLs.json" | ConvertFrom-Json
 
 # Define download folder
 $downloadFolder = "$Env:UserProfile\Downloads"
