ba7521792a9e95020c52bd13c5be9beb4fb22fbe
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-12-16 00:54:24 +0530

    Update script.ps1 🚀
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- .github/scripts/script.ps1
+++ .github/scripts/script.ps1
@@ -25,7 +25,7 @@ $baseUrl = "https://www.7-zip.org/"
 $softwareList += [PSCustomObject]@{
     appName = "7-Zip"
     version = ([regex]'Download 7-Zip ([\d.]+)').Match((Invoke-WebRequest $baseUrl -UseBasicParsing).Content).Groups[1].Value
-    url     = $baseUrl + ((Invoke-WebRequest $baseUrl -UseBasicParsing).Links | Where-Object { $_.Href -like '*x64.exe' } | Select-Object -First 1 -ExpandProperty Href)
+    url     = $baseUrl + (Invoke-WebRequest $baseUrl -UseBasicParsing).Links | Where-Object { $_.Href -like '*x64.exe' } | Select-Object -First 1 -ExpandProperty Href
 }
 
 # BleachBit
@@ -75,11 +75,11 @@ $softwareList += [PSCustomObject]@{
 }
 
 # IDM
-$baseUrl = "https://www.internetdownloadmanager.com/"
+$baseUrl = "https://www.internetdownloadmanager.com/news.html"
 
 $softwareList += [PSCustomObject]@{
     appName = "Internet Download Manager"
-    version = if ((Invoke-WebRequest $baseUrl'news.html' -UseBasicParsing).Content -match 'new in version (\d+\.\d+) Build (\d+)') { $matches[1] + '.' + $matches[2] }
+    version = (([regex]'new in version (\d+\.\d+) Build (\d+)').Match((Invoke-WebRequest $baseUrl -UseBasicParsing).Content).Groups[1, 2] -join '.')
     url     = (Invoke-WebRequest $baseUrl -UseBasicParsing).Links | Where-Object { $_.Href -like '*.exe' } | Select-Object -First 1 -ExpandProperty Href
 }
 
@@ -88,116 +88,148 @@ $baseUrl = "https://data.services.jetbrains.com/products/releases?code=IIC&lates
 
 $softwareList += [PSCustomObject]@{
     appName = "IntelliJ IDEA Community Edition"
-    version = (Invoke-RestMethod -Uri $baseUrl).IIC.version
-    url     = ((Invoke-RestMethod -Uri $baseUrl).IIC | Select-Object -ExpandProperty downloads).windows.link
+    version = (Invoke-RestMethod $baseUrl).IIC.version
+    url     = ((Invoke-RestMethod $baseUrl).IIC | Select-Object -ExpandProperty downloads).windows.link
 }
 
-$javaData = Get-ExistingData "Java(TM) SE Development Kit"
+# JDK
+$baseUrl = "https://www.oracle.com/java/technologies/downloads/"
+
 $softwareList += [PSCustomObject]@{
     appName = "Java(TM) SE Development Kit"
-    version = $javaData.version
+    version = ([regex]'JDK Development Kit (\d+\.\d+\.\d+)').Match((Invoke-WebRequest $baseUrl -UseBasicParsing).Content).Groups[1].Value
     url     = "https://download.oracle.com/java/21/latest/jdk-21_windows-x64_bin.exe"
 }
 
-$klcpData = Get-ExistingData "K-Lite Codec Pack"
+# K-Lite
+$baseUrl = "https://codecguide.com/download_k-lite_codec_pack_full.htm"
+
 $softwareList += [PSCustomObject]@{
     appName = "K-Lite Codec Pack"
-    version = $klcpData.version
-    url     = "$((Invoke-WebRequest -Uri 'https://codecguide.com/download_k-lite_codec_pack_full.htm' -UseBasicParsing).Links | Where-Object { $_.Href -like '*Full.exe' } | Select-Object -First 1 -ExpandProperty Href)"
+    version = ([regex]'Version (\d+\.\d+\.\d+) Full').Match((Invoke-WebRequest $baseUrl -UseBasicParsing).Content).Groups[1].Value
+    url     = (Invoke-WebRequest $baseUrl -UseBasicParsing).Links | Where-Object { $_.Href -like '*Full.exe' } | Select-Object -First 1 -ExpandProperty Href
 }
 
+# Node.js
 $baseUrl = "https://nodejs.org/download/release/latest/"
-$nodeData = Get-ExistingData "Node.js"
+
 $softwareList += [PSCustomObject]@{
     appName = "Node.js"
-    version = $nodeData.version
-    url     = "$baseUrl$((Invoke-WebRequest -Uri $baseUrl -UseBasicParsing).Links | Where-Object { $_.Href -like 'node*x64.msi' } | Select-Object -First 1 -ExpandProperty Href)"
+    version = ((Invoke-WebRequest $baseUrl -UseBasicParsing).Links | Where-Object { $_.Href -like 'node*x64.msi' } | Select-Object -First 1 -ExpandProperty Href) -replace '.*node-v(\d+\.\d+\.\d+).*', '$1'
+    url     = $baseUrl + (Invoke-WebRequest $baseUrl -UseBasicParsing).Links | Where-Object { $_.Href -like 'node*x64.msi' } | Select-Object -First 1 -ExpandProperty Href
 }
 
-$vsCodeData = Get-ExistingData "Microsoft Visual Studio Code"
+# VS Code
+$baseUrl = "https://api.github.com/repos/microsoft/vscode/releases/latest"
+$vscData = Get-ExistingData "Microsoft Visual Studio Code"
+
 $softwareList += [PSCustomObject]@{
     appName    = "Microsoft Visual Studio Code"
-    version    = $vsCodeData.version
+    version    = (Invoke-RestMethod $baseUrl).tag_name
     url        = "https://update.code.visualstudio.com/latest/win32-x64/stable"
-    extensions = $vsCodeData.extensions
+    extensions = $vscData.extensions
 }
 
-$nppData = Get-ExistingData "Notepad++"
+# Notepad++
+$baseUrl = "https://api.github.com/repos/notepad-plus-plus/notepad-plus-plus/releases/latest"
+
 $softwareList += [PSCustomObject]@{
     appName = "Notepad++"
-    version = $nppData.version
-    url     = "$((Invoke-RestMethod -Uri 'https://api.github.com/repos/notepad-plus-plus/notepad-plus-plus/releases/latest').assets | Where-Object { $_.name -like 'npp*x64.exe' } | Select-Object -ExpandProperty browser_download_url)"
+    version = (Invoke-RestMethod $baseUrl).tag_name.TrimStart('v')
+    url     = (Invoke-RestMethod $baseUrl).assets | Where-Object { $_.name -like 'npp*x64.exe' } | Select-Object -ExpandProperty browser_download_url
 }
 
-$obsdnData = Get-ExistingData "Obsidian"
+# Obsidian
+$baseUrl = "https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest"
+
 $softwareList += [PSCustomObject]@{
     appName = "Obsidian"
-    version = $obsdnData.version
-    url     = $((Invoke-RestMethod -Uri 'https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest').assets | Where-Object { $_.name -like 'Obsidian.*.exe' } | Select-Object -ExpandProperty browser_download_url)[3]
+    version = (Invoke-RestMethod $baseUrl).tag_name.TrimStart('v')
+    url     = ((Invoke-RestMethod $baseUrl).assets | Where-Object { $_.name -like 'Obsidian.*.exe' } | Select-Object -ExpandProperty browser_download_url)[3]
 }
 
-$ompData = Get-ExistingData "OhMyPosh"
+# OhMyPosh
+$baseUrl = "https://api.github.com/repos/JanDeDobbeleer/oh-my-posh/releases/latest"
+
 $softwareList += [PSCustomObject]@{
     appName = "OhMyPosh"
-    version = $ompData.version
-    url     = "$((Invoke-RestMethod -Uri 'https://api.github.com/repos/JanDeDobbeleer/oh-my-posh/releases/latest').assets | Where-Object { $_.name -eq 'install-amd64.exe' } | Select-Object -ExpandProperty browser_download_url)"
+    version = (Invoke-RestMethod $baseUrl).tag_name.TrimStart('v')
+    url     = (Invoke-RestMethod $baseUrl).assets | Where-Object { $_.name -eq 'install-amd64.exe' } | Select-Object -ExpandProperty browser_download_url
 }
 
-$ptnData = Get-ExistingData "Proton VPN"
+# Proton VPN
+$baseUrl = "https://protonvpn.com/download/windows-releases.json"
+$finlUrl = ((Invoke-RestMethod $baseUrl).Categories | Where-Object { $_.Name -eq 'Stable' } | Select-Object -ExpandProperty Releases)[0]
+
 $softwareList += [PSCustomObject]@{
     appName = "Proton VPN"
-    version = $ptnData.version
-    url     = $((Invoke-RestMethod -Uri 'https://protonvpn.com/download/windows-releases.json').Categories | Where-Object { $_.name -eq 'Stable' } | Select-Object -ExpandProperty Releases)[0].File.Url
+    version = $finlUrl.Version
+    url     = $finlUrl.File.Url
 }
 
-$pyData = Get-ExistingData "Python"
+# Python
+$baseUrl = "https://www.python.org/downloads/"
+
 $softwareList += [PSCustomObject]@{
     appName = "Python"
-    version = $pyData.version
-    url     = "$((Invoke-WebRequest -Uri 'https://www.python.org/downloads/' -UseBasicParsing).Links | Where-Object { $_.Href -like '*amd64.exe' } | Select-Object -First 1 -ExpandProperty Href)"
+    version = ([regex]'python-(\d+\.\d+\.\d+)').Match((Invoke-WebRequest $baseUrl -UseBasicParsing).Content).Groups[1].Value
+    url     = (Invoke-WebRequest $baseUrl -UseBasicParsing).Links | Where-Object { $_.Href -like '*amd64.exe' } | Select-Object -First 1 -ExpandProperty Href
 }
 
-$revoData = Get-ExistingData "Revo Uninstaller Pro"
+# Revo Uninstaller
+$baseUrl = "https://www.revouninstaller.com/revo-uninstaller-pro-full-version-history/"
+
 $softwareList += [PSCustomObject]@{
     appName = "Revo Uninstaller Pro"
-    version = $revoData.version
-    url     = "https://download.revouninstaller.com/download/RevoUninProSetup.exe"
+    version = ([regex]'VERSION (\d+\.\d+\.\d+)').Match((Invoke-WebRequest $baseUrl -UseBasicParsing).Content).Groups[1].Value
+    url     = (Invoke-WebRequest $baseUrl -UseBasicParsing).Links | Where-Object { $_.Href -like '*RevoUninProSetup.exe' } | Select-Object -First 1 -ExpandProperty Href
 }
 
-$sabData = Get-ExistingData "StartAllBack"
+# StartAllBack
+$baseUrl = "https://www.startallback.com/"
+
 $softwareList += [PSCustomObject]@{
     appName = "StartAllBack"
-    version = $sabData.version
-    url     = "https://www.startallback.com/download.php"
+    version = ([regex]'Download v(\d+\.\d+\.\d+)').Match((Invoke-WebRequest $baseUrl -UseBasicParsing).Content).Groups[1].Value
+    url     = $baseUrl + 'download.php'
 }
 
-$sibData = Get-ExistingData "StartIsBack++"
+# StartIsBack++
+$baseUrl = "https://www.startisback.com/#download-tab/"
+
 $softwareList += [PSCustomObject]@{
     appName = "StartIsBack++"
-    version = $sibData.version
-    url     = "https://startisback.sfo3.cdn.digitaloceanspaces.com/StartIsBackPlusPlus_setup.exe"
+    version = ([regex]'StartIsBack\+\+ (\d+\.\d+\.\d+)').Match((Invoke-WebRequest $baseUrl -UseBasicParsing).Content).Groups[1].Value
+    url     = (Invoke-WebRequest $baseUrl -UseBasicParsing).Links | Where-Object { $_.Href -like '*setup.exe' } | Select-Object -First 1 -ExpandProperty Href
 }
 
-$tgData = Get-ExistingData "Telegram"
+# Telegram
+$baseUrl = "https://telegram.org/dl/desktop/win64"
+$finlUrl = (Invoke-WebRequest $baseUrl -MaximumRedirection 0 -UseBasicParsing -ErrorAction SilentlyContinue).Headers.Location
+
 $softwareList += [PSCustomObject]@{
     appName = "Telegram"
-    version = $tgData.version
-    url     = "https://telegram.org/dl/desktop/win64"
+    version = $finlUrl -split 'tsetup-x64.' -split '.exe' | Select-Object -Index 1
+    url     = $finlUrl
 }
 
+# VLC
 $baseUrl = "https://get.videolan.org/vlc/last/win64/"
-$vlcData = Get-ExistingData "VLC media player"
+$finlUrl = (Invoke-WebRequest $baseUrl -UseBasicParsing).Links | Where-Object { $_.Href -like 'vlc*win64.exe' } | Select-Object -First 1 -ExpandProperty Href
+
 $softwareList += [PSCustomObject]@{
     appName = "VLC media player"
-    version = $vlcData.version
-    url     = "$baseUrl$((Invoke-WebRequest -Uri $baseUrl -UseBasicParsing).Links | Where-Object { $_.Href -like 'vlc*.exe' } | Select-Object -First 1 -ExpandProperty Href)"
+    version = $finlUrl
+    url     = $baseUrl + $finlUrl
 }
 
-$ytData = Get-ExistingData "YoutubeDownloader"
+# YoutubeDownloader
+$baseUrl = "https://api.github.com/repos/Tyrrrz/YoutubeDownloader/releases/latest"
+
 $softwareList += [PSCustomObject]@{
     appName = "YoutubeDownloader"
-    version = $ytData.version
-    url     = "$((Invoke-RestMethod -Uri 'https://api.github.com/repos/Tyrrrz/YoutubeDownloader/releases/latest').assets | Where-Object { $_.name -eq 'YoutubeDownloader.zip' } | Select-Object -ExpandProperty browser_download_url)"
+    version = (Invoke-RestMethod $baseUrl).tag_name
+    url     = ((Invoke-RestMethod $baseUrl).assets | Where-Object { $_.name -eq 'YoutubeDownloader.zip' } | Select-Object -ExpandProperty browser_download_url)
 }
 
 # Convert the software list to JSON
@@ -205,6 +237,3 @@ $json = $softwareList | ConvertTo-Json -Depth 10
 
 # Output the JSON to a file
 $json | Out-File -FilePath $jsonPath
-
-# Optionally, write the JSON to the console
-Write-Host $json
