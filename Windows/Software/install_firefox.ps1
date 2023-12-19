1df8334ac589c760a8b0b0dc33cd5a79e61caf3e
sakshiagrwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-12-19 19:18:39 +0530

    Update install_firefox.ps1

--- Windows/Software/install_firefox.ps1
+++ Windows/Software/install_firefox.ps1
@@ -2,28 +2,27 @@
 $firefoxSetupUrl = "https://releases.mozilla.org/pub/firefox/releases/120.0.1/win64/en-GB/Firefox%20Setup%20120.0.1.exe"
 $hashFileUrl = "https://ftp.mozilla.org/pub/firefox/releases/120.0.1/SHA512SUMS"
 
-# Define the paths for the downloaded files
-$firefoxSetupPath = "C:\Users\Deepak\Desktop\Scripts\FirefoxSetup.exe"
-$hashFilePath = "C:\Users\Deepak\Desktop\Scripts\SHA512SUMS"
+# Define the path for the downloaded setup file
+$firefoxSetupPath = "$Env:UserProfile\Desktop\Scripts\FirefoxSetup.exe"
 
 # Download the Firefox setup file
-# Invoke-WebRequest -Uri $firefoxSetupUrl -OutFile $firefoxSetupPath
+Invoke-WebRequest -Uri $firefoxSetupUrl -OutFile $firefoxSetupPath
 
-# Download the SHA512 hash file
-# Invoke-WebRequest -Uri $hashFileUrl -OutFile $hashFilePath
+# Fetch the content of the SHA512 hash file
+$hashFileContent = Invoke-RestMethod -Uri $hashFileUrl
 
-# Extract the hash for the setup file from the SHA512SUMS file
-$expectedHash = (Get-Content $hashFilePath | Select-String -Pattern "win64/en-GB/Firefox Setup 120.0.1.exe" | ForEach-Object { ($_ -split ' ')[0] }).Trim()
+# Extract the hash for the setup file from the content
+$expectedHash = ($hashFileContent -split "`n" | Select-String -Pattern "win64/en-GB/Firefox Setup 120.0.1.exe").Line.Split(" ")[0].Trim()
 
 # Compute the SHA512 hash of the downloaded setup file
 $computedHash = (Get-FileHash -Path $firefoxSetupPath -Algorithm SHA512).Hash
 
 # Compare the computed hash with the expected hash
 if ($computedHash -eq $expectedHash) {
-    # If the hashes match, output an success message
+    # If the hashes match, output a success message
     Write-Host "Hash matches the expected hash" -ForegroundColor Green
-    Write-Host $computedHash -ForegroundColor Yellow
-    Write-Host $expectedHash -ForegroundColor Blue
+    Write-Host "Computed Hash: " $computedHash -ForegroundColor Yellow
+    Write-Host "Expected Hash: " $expectedHash -ForegroundColor Blue
 }
 else {
     # If the hashes do not match, output an error message
