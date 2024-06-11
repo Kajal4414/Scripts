d9c468125f9f1bc6949317d3c11e0e3ad4eeddab
Sakshi Agrawal <81718060+sakshiagrwal@users.noreply.github.com>
2024-06-11 19:42:07 +0530

    Create charging_threshold.ps1

---
+++ Windows/Extra/charging_threshold.ps1
@@ -0,0 +1,20 @@
+# Check if ASUSOptimization service exists
+if (-not (Get-Service -Name "ASUSOptimization" -ErrorAction SilentlyContinue)) {
+    Write-Host "ASUSOptimization service does not exist. Install the 'ASUS System Control Interface v3' drivers and try again."
+    Exit
+}
+
+# Set the battery charging threshold to 80%
+reg add "HKLM\SOFTWARE\ASUS\ASUS System Control Interface\AsusOptimization\ASUS Keyboard Hotkeys" /v ChargingRate /t REG_DWORD /d 80 /f
+
+# Get all ASUS services except ASUSOptimization and disable them
+Get-Service | Where-Object { $_.Name -like "*ASUS*" -and $_.Name -ne "ASUSOptimization" } | ForEach-Object {
+    Set-Service -Name $_.Name -StartupType Disabled -ErrorAction SilentlyContinue
+    Write-Host "Disabled $($_.Name)"
+}
+
+# Restart the ASUSOptimization service
+Restart-Service -Name "ASUSOptimization"
+
+# Print a message indicating the battery charging threshold has been set
+Write-Host "Battery charging threshold set to 80%."
