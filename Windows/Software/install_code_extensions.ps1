5945b8d64922698107ee3f1c02c8ec8fb5014c47
Sakshi Agrawal <81718060+sakshiagrwal@users.noreply.github.com>
2024-06-18 22:02:05 +0530

    software: add install_code_extensions.ps1 revert commit ce77c03
    Change-Id: I47c57790fd7d628b768cdb38d813932a8ea4a145
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

---
+++ Windows/Software/install_code_extensions.ps1
@@ -0,0 +1,35 @@
+# List of extension IDs
+$extensions = @(
+    "beardedbear.beardedtheme",
+    "dbaeumer.vscode-eslint",
+    "eamodio.gitlens",
+    "esbenp.prettier-vscode",
+    "foxundermoon.shell-format",
+    "github.codespaces",
+    "github.github-vscode-theme",
+    "jock.svg",
+    "ms-python.black-formatter",
+    "ms-python.debugpy",
+    "ms-python.pylint",
+    "ms-python.python",
+    "ms-python.vscode-pylance",
+    "ms-vscode.makefile-tools",
+    "ms-vscode.powershell",
+    "pkief.material-icon-theme",
+    "redhat.java",
+    "redhat.vscode-xml",
+    "redhat.vscode-yaml",
+    "ritwickdey.liveserver",
+    "tabnine.tabnine-vscode",
+    "visualstudioexptteam.intellicode-api-usage-examples",
+    "visualstudioexptteam.vscodeintellicode",
+    "vscjava.vscode-java-debug",
+    "vscjava.vscode-java-dependency",
+    "vscjava.vscode-java-pack",
+    "vscjava.vscode-java-test",
+    "vscjava.vscode-maven"
+)
+
+foreach ($extensionId in $extensions) {
+    code --install-extension $extensionId
+}
