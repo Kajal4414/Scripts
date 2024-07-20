aeaa150a2d4c8e55c379f8fbf037a0e56afdd46f
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-20 18:38:28 +0530

    Create mpv.ps1
    Change-Id: I39c8ae5a57384373cf77aeef43750f3da5bc0f0d
    Signed-off-by: Kajal4414 <81718060+Kajal4414@users.noreply.github.com>

---
+++ Windows/Software/mpv.ps1
@@ -0,0 +1,37 @@
+# Check for Administrator privileges
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Error: Administrator privileges required." -ForegroundColor Red
+    exit 1
+}
+
+# Define URLs and paths
+$BaseUrl = "https://sourceforge.net/projects/mpv-player-windows/files/64bit-v3"
+$TempPath = Join-Path -Path $env:TEMP -ChildPath "mpv.7z"
+$InstallPath = Join-Path -Path $env:PROGRAMFILES -ChildPath "MPV"
+
+# Check if mpv is already installed
+$MpvPath = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\mpv.exe" -ErrorAction SilentlyContinue)."(Default)"
+if ($MpvPath -and (Test-Path $MpvPath)) {
+    Write-Host "Already installed mpv $((Get-Item $MpvPath).VersionInfo.ProductVersion)." -ForegroundColor Green
+    exit 0
+}
+
+# Get download URL
+$DownloadUrl = ([regex]::Matches((Invoke-WebRequest $BaseUrl -UseBasicParsing).Content, "https://.*?download") | Select-Object -First 1).Value
+
+# Download and install mpv
+Write-Host "Downloading mpv..." -ForegroundColor Yellow
+try {
+    curl.exe -LS -o $TempPath $DownloadUrl
+    Write-Host "Download successful." -ForegroundColor Green
+
+    Write-Host "Installing mpv..." -ForegroundColor Yellow
+    nanazipg x "$TempPath" -o"$InstallPath" -y
+    Start-Process -FilePath "$InstallPath\installer\mpv-install.bat" -Wait -NoNewWindow
+} catch {
+    Write-Host "Error: $_" -ForegroundColor Red
+    exit 1
+}
+
+# Cleanup
+Remove-Item -Path $TempPath -Force -ErrorAction SilentlyContinue
