d17b2c17dcb708c1771dd33c4ef30569fc079f74
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-25 05:34:56 +0530

    Update `install_firefox.ps1` script form `dev` branch

--- Windows/Software/install_firefox.ps1
+++ Windows/Software/install_firefox.ps1
@@ -8,177 +8,141 @@ param (
     [string]$version
 )
 
-# Function to pause and wait for user input
+# Function to pause the script and wait for user input
 function PauseNull {
     Write-Host "Press any key to exit... " -NoNewline
     $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown') | Out-Null
     exit
 }
 
-# Check for admin privileges
-$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-if (-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+# Check for Administrator privileges
+if (-not (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Host "Error: Administrator privileges required." -ForegroundColor Red
     PauseNull
 }
 
-# Check for internet connectivity
+# Check for internet connection
 if (-not (Test-Connection 8.8.8.8 -Count 1 -Quiet)) {
     Write-Host "Error: Internet connection required." -ForegroundColor Red
     PauseNull
 }
 
-# Main script execution
+# Function to download files using curl with optional parameters
+function DownloadFile($url, $path, $useLSs = $false) {
+    curl.exe -o $path $(if ($useLSs) { "-LSs" } else { "-S" }) $url
+    Write-Host "Downloaded: $path" -ForegroundColor Green
+}
+
+# Function to verify file hash
+function VerifyHash($file, $hashSource, $remoteFile) {
+    $localHash = (Get-FileHash -Path $file -Algorithm SHA512).Hash
+    $remoteHash = (Invoke-RestMethod -Uri $hashSource).Split("`n") | Select-String -Pattern $remoteFile | ForEach-Object { $_.Line.Split(" ")[0].Trim() }
+    return $localHash -eq $remoteHash
+}
+
+# Function to configure additional files for Firefox
+function ConfigureFiles($installDir) {
+    New-Item -Path "$installDir\distribution" -ItemType Directory -Force | Out-Null
+    $files = @(
+        @{Name = "policies.json"; Url = "https://raw.githubusercontent.com/sakshiagrwal/Scripts/main/Windows/Extra/policies.json"; Path = "$installDir\distribution\policies.json" },
+        @{Name = "autoconfig.js"; Url = "https://raw.githubusercontent.com/sakshiagrwal/Scripts/main/Windows/Extra/autoconfig.js"; Path = "$installDir\defaults\pref\autoconfig.js" },
+        @{Name = "firefox.cfg"; Url = "https://raw.githubusercontent.com/sakshiagrwal/Scripts/main/Windows/Extra/firefox.cfg"; Path = "$installDir\firefox.cfg" }
+    )
+    foreach ($file in $files) {
+        if (-not (Test-Path $file.Path)) {
+            DownloadFile $file.Url $file.Path $true
+        }
+    }
+}
+
+# Main function to control the installation process
 function main {
-    Write-Host "Starting Firefox installation process..." -ForegroundColor Yellow
-
-    # Attempt to fetch JSON data
-    try {
-        $response = Invoke-RestMethod -Uri "https://product-details.mozilla.org/1.0/firefox_versions.json"
+    # Fetch JSON data from Mozilla's product details API
+    try { 
+        $response = Invoke-RestMethod -Uri "https://product-details.mozilla.org/1.0/firefox_versions.json" 
     }
-    catch {
+    catch { 
         Write-Host "Failed to fetch JSON data: $_" -ForegroundColor Red
-        PauseNull
+        PauseNull 
     }
 
-    # Determine the version to download based on the specified edition
-    $remoteVersion = switch ($edition) {
-        "Developer" { $response.FIREFOX_DEVEDITION }
-        "Enterprise" { $response.FIREFOX_ESR }
-        default { $response.LATEST_FIREFOX_VERSION }
-    }
-
-    # Use the specified version if provided, otherwise use the determined version
-    $remoteVersion = if ($version) { $version } else { $remoteVersion }
-
-    # Define download URL and file paths
+    # Determine the remote version based on the provided edition or default to latest
+    $remoteVersion = if ($version) { $version } else { switch ($edition) { "Developer" { $response.FIREFOX_DEVEDITION }; "Enterprise" { $response.FIREFOX_ESR }; default { $response.LATEST_FIREFOX_VERSION } } }
     $downloadUrl = "https://releases.mozilla.org/pub/firefox/releases/$remoteVersion/win64/$lang/Firefox%20Setup%20$remoteVersion.exe"
     $hashSource = "https://ftp.mozilla.org/pub/firefox/releases/$remoteVersion/SHA512SUMS"
     $installDir = "$env:PROGRAMFILES\Mozilla Firefox"
     $setupFile = "$env:TEMP\Firefox Setup $remoteVersion.exe"
 
-    # Check if the current version is already installed
-    if (Test-Path "$installDir\firefox.exe" -PathType Leaf) {
+    # Check if Firefox is already installed and the version matches
+    if (Test-Path "$installDir\firefox.exe") {
         $localVersion = (Get-Item "$installDir\firefox.exe").VersionInfo.ProductVersion
-
-        if ($localVersion -eq $remoteVersion) {
+        if ($localVersion -eq $remoteVersion -and -not $force) {
             Write-Host "Mozilla Firefox v$remoteVersion is already installed." -ForegroundColor Green
-
-            if ($force) {
-                Write-Warning "-force specified, proceeding anyway."
-            }
-            else {
-                PauseNull
-            }
-        }
-    }
-
-    # Download Firefox setup file
-    Write-Host "`nDownloading Mozilla Firefox v$remoteVersion setup..." -ForegroundColor Yellow
-    curl.exe -o $setupFile -S $downloadUrl
-    Write-Host "Downloading successful." -ForegroundColor Green
-
-    # Verify hash if not skipping hash check
-    if (-not $skipHashCheck) {
-        Write-Host "`nVerifying SHA-512 Hash..." -ForegroundColor Yellow
-        $localSHA512 = (Get-FileHash -Path $setupFile -Algorithm SHA512).Hash
-        $remoteSHA512 = (Invoke-RestMethod -Uri $hashSource).Split("`n") | Select-String -Pattern "win64/$lang/Firefox Setup $remoteVersion.exe" | ForEach-Object { $_.Line.Split(" ")[0].Trim() }
-
-        if ($localSHA512 -eq $remoteSHA512) {
-            Write-Host "SHA-512 Hash verification successful." -ForegroundColor Green
-        }
-        else {
-            Write-Host "SHA-512 Hash verification failed, consider using -skipHashCheck." -ForegroundColor Red
             PauseNull
         }
     }
 
-    # Installation process
+    # Download Firefox setup file
+    Write-Host "Downloading Mozilla Firefox v$remoteVersion setup..." -ForegroundColor Yellow
+    DownloadFile $downloadUrl $setupFile
+
+    # Verify SHA-512 hash of the downloaded setup file
+    Write-Host "`nVerifying SHA-512 Hash..." -ForegroundColor Yellow
+    if (-not $skipHashCheck -and -not (VerifyHash $setupFile $hashSource "win64/$lang/Firefox Setup $remoteVersion.exe")) {
+        Write-Host "SHA-512 Hash verification failed, consider using -skipHashCheck." -ForegroundColor Red
+        PauseNull
+    } else {
+        Write-Host "Verification Successful." -ForegroundColor Green
+    }
+
+    # Install Firefox
     Write-Host "`nInstalling Mozilla Firefox..." -ForegroundColor Yellow
     Stop-Process -Name "firefox" -ErrorAction SilentlyContinue
-
-    try {
+    try { 
         Start-Process -FilePath $setupFile -ArgumentList "/S /MaintenanceService=false" -Wait
-        Write-Host "Installation successful." -ForegroundColor Green
+        Write-Host "Installation Successful." -ForegroundColor Green 
     }
-    catch {
+    catch { 
         Write-Host "Error occurred while installing 'Mozilla Firefox $remoteVersion.exe': $_" -ForegroundColor Red
-        PauseNull
-    }
-
-    Write-Host "`nRemoving unnecessary files..." -ForegroundColor Yellow
-
-    # Remove Firefox setup file
-    if (Test-Path $setupFile -PathType Leaf) {
-        Write-Host "Removed: Mozilla Firefox $remoteVersion.exe" -ForegroundColor Green
-        Remove-Item $setupFile
+        PauseNull 
     }
 
     # Remove unnecessary files
-    $removeFiles = @(
-        "crashreporter.exe",
-        "crashreporter.ini",
-        "defaultagent.ini",
-        "defaultagent_localized.ini",
-        "default-browser-agent.exe",
-        "maintenanceservice.exe",
-        "maintenanceservice_installer.exe",
-        "minidump-analyzer.exe",
-        "pingsender.exe",
-        "updater.exe",
-        "updater.ini",
-        "update-settings.ini"
-    )
-
-    $removeFiles | ForEach-Object {
+    Write-Host "`nRemoving unnecessary files..." -ForegroundColor Yellow
+    Remove-Item $setupFile -ErrorAction SilentlyContinue
+    "crashreporter.exe crashreporter.ini defaultagent.ini defaultagent_localized.ini default-browser-agent.exe maintenanceservice.exe maintenanceservice_installer.exe minidump-analyzer.exe pingsender.exe updater.exe updater.ini update-settings.ini".Split() | ForEach-Object {
         $filePath = "$installDir\$_"
-        if (Test-Path $filePath -PathType Leaf) {
-            Write-Host "Removed: $_" -ForegroundColor Green
-            Remove-Item $filePath
+        if (Test-Path $filePath) { 
+            Remove-Item $filePath -ErrorAction SilentlyContinue
+            Write-Host "Removed: $filePath" -ForegroundColor Green 
         }
     }
 
-    # Configuration settings
-    if ($configs) {
-        Write-Host "`nConfiguring Mozilla Firefox settings..." -ForegroundColor Yellow
-
-        # Create 'distribution' folder for policies.json
-        New-Item -Path $installDir -Name "distribution" -ItemType Directory -Force | Out-Null
-
-        # Write policies.json
-        $policyFile = "$installDir\distribution\policies.json"
-        if (-not (Test-Path $policyFile)) {
-            curl.exe -o $policyFile -LSs "https://raw.githubusercontent.com/sakshiagrwal/Scripts/main/Windows/Extra/policies.json"
-            Write-Host "Created: policies.json" -ForegroundColor Green
-        }
-
-        # Write autoconfig.js
-        $autoconfigFile = "$installDir\defaults\pref\autoconfig.js"
-        if (-not (Test-Path $autoconfigFile)) {
-            curl.exe -o $autoconfigFile -LSs "https://raw.githubusercontent.com/sakshiagrwal/Scripts/main/Windows/Extra/autoconfig.js"
-            Write-Host "Created: autoconfig.js" -ForegroundColor Green
-        }
-
-        # Write firefox.cfg
-        $firefoxCfgFile = "$installDir\firefox.cfg"
-        if (-not (Test-Path $firefoxCfgFile)) {
-            curl.exe -o $firefoxCfgFile -LSs "https://raw.githubusercontent.com/sakshiagrwal/Scripts/main/Windows/Extra/firefox.cfg"
-            Write-Host "Created: firefox.cfg" -ForegroundColor Green
-        }
+    # Configure Firefox settings if requested
+    if ($configs) { 
+        Write-Host "`nConfiguring Firefox Settings..." -ForegroundColor Yellow
+        ConfigureFiles $installDir 
     }
 
-    # Firefox Theme
+    # Install Firefox Mod Blur Theme if requested
     if ($theme) {
+        Write-Host "`nInstalling Firefox Mod Blur Theme..." -ForegroundColor Yellow
         $profilePath = (Get-Item "$env:APPDATA\Mozilla\Firefox\Profiles\*.default-release").FullName
-        if (-not (Test-Path "$profilePath\chrome") -and (Get-Command "git" -ErrorAction SilentlyContinue)) {
-            Write-Host "`nInstalling 'Firefox Mod Blur' Theme..." -ForegroundColor Yellow
+        if (-not $profilePath) { 
+            Start-Process "firefox.exe"
+            Start-Sleep -Seconds 3
+            Stop-Process -Name "firefox" -Force 
+        }
+        if ($profilePath -and (Test-Path "$profilePath\chrome")) { 
+            Write-Host "Skipping: Firefox Mod Blur Theme Already Installed." -ForegroundColor Green 
+        }
+        elseif ($profilePath -and -not (Test-Path "$profilePath\chrome") -and (Get-Command "git" -ErrorAction SilentlyContinue)) {
             git clone --depth 1 -q https://github.com/datguypiko/Firefox-Mod-Blur "$profilePath\chrome"
-            Remove-Item "$profilePath\chrome\*" -Exclude "ASSETS", "userChrome.css", "userContent.css" -Force -Recurse
-            Write-Host "Theme installed at default profile path '$profilePath\chrome'" -ForegroundColor Green
+            Get-ChildItem -Path "$profilePath\chrome" -Exclude "ASSETS", "userChrome.css", "userContent.css" -Force | Remove-Item -Force -Recurse
+            Write-Host "Installation Successful." -ForegroundColor Green
         }
     }
 
-    # Display release notes URL
     Write-Host "`nRelease notes: https://www.mozilla.org/en-US/firefox/$remoteVersion/releasenotes" -ForegroundColor Green
     return 0
 }
