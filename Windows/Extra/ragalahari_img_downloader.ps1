3f58434130a957f46f7080189e2dd550e82d1e86
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-08-01 17:36:02 +0530

    ragalahari_img_downloader: local

--- Windows/Extra/ragalahari_img_downloader.ps1
+++ Windows/Extra/ragalahari_img_downloader.ps1
@@ -1,6 +1,6 @@
 function Get-NumberOfImages($totalImages) {
     do {
-        $userInput = Read-Host "Enter the number of images to download (Press Enter for all '$totalImages')"
+        $userInput = Read-Host "Enter the number of images to download (Press Enter for all '$totalImages')"; Write-Host
         if (-not $userInput) { return $totalImages }
         $inputAsInt = $userInput -as [int]
         if ($inputAsInt -and $inputAsInt -gt 0 -and $inputAsInt -le $totalImages) { return $inputAsInt }
@@ -12,18 +12,30 @@ function DownloadImage($url, $destinationFolder) {
     $fileName = [System.IO.Path]::GetFileName($url)
     $filePath = Join-Path $destinationFolder $fileName
     if (-not (Test-Path -Path $filePath)) {
-        Write-Host "Downloading '$fileName'..."
-        $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri $url -OutFile $filePath -ErrorAction Stop
-        return "downloaded"
+        Write-Host -NoNewline "$fileName - Downloading..." -ForegroundColor Blue
+        $ProgressPreference = 'SilentlyContinue'
+        try {
+            Invoke-WebRequest -Uri $url -OutFile $filePath -ErrorAction Stop
+            Write-Host "`r$fileName - Downloaded.   " -ForegroundColor Green
+            return "downloaded"
+        } catch {
+            Write-Host "`r$fileName - Failed.       " -ForegroundColor Red
+            return "failed"
+        }
     } else {
-        Write-Host "'$fileName' already exists." -ForegroundColor Yellow
+        Write-Host "$fileName - Skipped." -ForegroundColor Yellow
         return "skipped"
     }
 }
 
+function Get-FolderNameFromUrl($url) {
+    $folderName = $url -replace '.*/([^/]+)\.aspx$', '$1' -replace '-', ' '
+    return (Get-Culture).TextInfo.ToTitleCase($folderName.ToLower())
+}
+
 do {
     $userUrl = Read-Host "Enter the gallery URL (Press Enter for default)"
-    $userUrl = if ([string]::IsNullOrWhiteSpace($userUrl)) { "https://www.ragalahari.com/actor/171464/allu-arjun-at-honer-richmont-launch.aspx" } else { $userUrl }
+    $userUrl = if ([string]::IsNullOrWhiteSpace($userUrl)) { "https://www.ragalahari.com/actress/173348/pragati-srivastava-at-gam-gam-ganesha-pre-release-event-hd-gallery.aspx" } else { $userUrl }
     $isValidUrl = $userUrl -match '\.aspx$'
     if (-not $isValidUrl) { Write-Host "Invalid URL. Please enter a valid gallery URL." -ForegroundColor Red }
 } while (-not $isValidUrl)
@@ -33,16 +45,21 @@ $imageUrls = [regex]::Matches($response, '(?<=src=")[^"]*t\.jpg') | ForEach-Obje
 $totalImages = $imageUrls.Count
 $numberOfImages = Get-NumberOfImages $totalImages
 
-$destinationFolder = Read-Host "Enter the destination folder name"
-if (-not (Test-Path -Path $destinationFolder)) { New-Item -ItemType Directory -Path $destinationFolder -Force | Out-Null }
+$destinationFolder = Get-FolderNameFromUrl $userUrl
+if (-not (Test-Path -Path $destinationFolder)) { New-Item -ItemType Directory -Path $destinationFolder -Force | Out-Null; Write-Host "$destinationFolder - Folder Created." -ForegroundColor Green; Write-Host }
 
 $downloadedCount = 0
 $skippedCount = 0
+$failedCount = 0
 
 foreach ($imageUrl in $imageUrls[0..($numberOfImages - 1)]) {
     $status = DownloadImage $imageUrl $destinationFolder
     if ($status -eq "downloaded") { $downloadedCount++ }
-    if ($status -eq "skipped") { $skippedCount++ }
+    elseif ($status -eq "skipped") { $skippedCount++ }
+    elseif ($status -eq "failed") { $failedCount++ }
 }
 
-Write-Host "`nAll images downloaded at '$(Resolve-Path -Path $destinationFolder)', Total downloaded: $downloadedCount, Total skipped: $skippedCount" -ForegroundColor Green
+Write-Host "`nImages downloaded at '$(Resolve-Path -Path $destinationFolder)'" -ForegroundColor Cyan -NoNewline
+Write-Host "`nDownloaded: $downloadedCount" -ForegroundColor Green -NoNewline
+Write-Host " - Skipped: $skippedCount" -ForegroundColor Yellow -NoNewline
+Write-Host " - Failed: $failedCount" -ForegroundColor Red
