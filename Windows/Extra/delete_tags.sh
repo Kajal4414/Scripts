6791db2079fac5cad69664eb2f9f80587b8e6d6a
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-08-16 06:54:27 +0530

    delete_tags: format

--- Windows/Extra/delete_tags.sh
+++ Windows/Extra/delete_tags.sh
@@ -5,12 +5,12 @@ REPO_NAME="your_repository_name"
 
 # Get all tags
 curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" \
-https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/git/refs/tags \
-| jq '.[] | .ref' \
-| while read tag_ref; do
-    # Extract tag name
-    tag_name=$(echo "$tag_ref" | awk -F '/' '{print $3}' | tr -d '"')
-    # Delete each tag
-    curl -X DELETE -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" \
-    https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/git/refs/tags/$tag_name
-  done
+    https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/git/refs/tags |
+    jq '.[] | .ref' |
+    while read tag_ref; do
+        # Extract tag name
+        tag_name=$(echo "$tag_ref" | awk -F '/' '{print $3}' | tr -d '"')
+        # Delete each tag
+        curl -X DELETE -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" \
+            https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/git/refs/tags/$tag_name
+    done
