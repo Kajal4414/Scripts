e194822bf487b15ba5cad2e3eebf2bcc6c2336e9
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-11-17 22:25:43 +0530

    Update charging.ps1

--- Windows/Extra/charging.ps1
+++ Windows/Extra/charging.ps1
@@ -2,13 +2,18 @@
 $currentThreshold = (Get-ItemProperty -Path "HKLM:\SOFTWARE\ASUS\ASUS System Control Interface\AsusOptimization\ASUS Keyboard Hotkeys" -ErrorAction Stop).ChargingRate
 Write-Host "Current Battery Charging Limit: $currentThreshold%`n" -ForegroundColor Cyan
 
-# Charging threshold
+# Charging threshold to be set
 $chargingThreshold = 40
 
-# Set charging threshold in registry
-Set-ItemProperty -Path "HKLM:\SOFTWARE\ASUS\ASUS System Control Interface\AsusOptimization\ASUS Keyboard Hotkeys" -Name ChargingRate -Value $chargingThreshold -ErrorAction Stop
-Write-Host "`nBattery Charging Limit Set To $chargingThreshold% In Registry" -ForegroundColor Green
+# Check if the current threshold is already 40
+if ($currentThreshold -ne $chargingThreshold) {
+    # Set charging threshold in registry
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\ASUS\ASUS System Control Interface\AsusOptimization\ASUS Keyboard Hotkeys" -Name ChargingRate -Value $chargingThreshold -ErrorAction Stop
+    Write-Host "`nBattery Charging Limit Set To $chargingThreshold% In Registry" -ForegroundColor Green
 
-# Restart ASUSOptimization service
-Restart-Service -Name "ASUSOptimization" -ErrorAction Stop
-Write-Host "`nASUSOptimization Service Restarted" -ForegroundColor Green
+    # Restart ASUSOptimization service
+    Restart-Service -Name "ASUSOptimization" -ErrorAction Stop
+    Write-Host "`nASUSOptimization Service Restarted" -ForegroundColor Green
+} else {
+    Write-Host "`nNo changes made. Battery Charging Limit is already set to $chargingThreshold%." -ForegroundColor Yellow
+}
