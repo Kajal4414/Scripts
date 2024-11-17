d6be11e8e7e37136f7b104bc71e53abc9b91525a
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-11-17 22:31:44 +0530

    Update charging.ps1

--- Windows/Extra/charging.ps1
+++ Windows/Extra/charging.ps1
@@ -1,11 +1,10 @@
-# Get current charging limit from registry
 $currentThreshold = (Get-ItemProperty -Path "HKLM:\SOFTWARE\ASUS\ASUS System Control Interface\AsusOptimization\ASUS Keyboard Hotkeys" -ErrorAction Stop).ChargingRate
 $chargingThreshold = 40
 
 if ($currentThreshold -ne $chargingThreshold) {
     Set-ItemProperty -Path "HKLM:\SOFTWARE\ASUS\ASUS System Control Interface\AsusOptimization\ASUS Keyboard Hotkeys" -Name ChargingRate -Value $chargingThreshold -ErrorAction Stop
     Restart-Service -Name "ASUSOptimization" -ErrorAction Stop
-    Write-Host "Battery Charging Limit updated to $chargingThreshold% and ASUSOptimization service restarted." -ForegroundColor Green
+    Write-Host "`nBattery Charging Limit updated to $chargingThreshold% and ASUSOptimization service restarted." -ForegroundColor Green
 } else {
-    Write-Host "Battery Charging Limit is already set to $chargingThreshold%. No changes made." -ForegroundColor Yellow
+    Write-Host "`nBattery Charging Limit is already set to $chargingThreshold%. No changes made." -ForegroundColor Yellow
 }
